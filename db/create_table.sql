d5368b58893c352069a5aff85c2115a5b170eb4c
spikeburton <ralphburton3@gmail.com>
2019-02-07 20:15:16 -0500

    add SQL migrations

---
+++ db/create_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE cats (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  breed TEXT
+);
+
+INSERT INTO cats (name, age, breed) VALUES ("Maru", 3, "Scottish Fold");
+INSERT INTO cats (name, age, breed) VALUES ( "Hana", 1, "Tabby");
+INSERT INTO cats (name, age, breed) VALUES ("Lil' Bub", 5, "American Shorthair");
+INSERT INTO cats (name, age, breed) VALUES ("Moe", 10, "Tabby");
+INSERT INTO cats (name, age, breed) VALUES ("Patches", 2, "Calico");
